3bf3e7f961fed63008ab8fd88352f20b179486bb
Dastan-Oskonbaev <dastiw1910@gmail.com>
2023-10-26 13:46:39 +0600

    Added models and admin.py

--- apps/cleaning/admin.py
+++ apps/cleaning/admin.py
@@ -1,3 +1,110 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact, ServicesCategory, OurServices, AboutUs, Reviews
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = (
+        'email',
+        'instagram',
+        'tiktok',
+        'telegram',
+        'whatsapp',
+        'phone_number',
+        'address',
+    )
+    list_display_links = (
+        'email',
+    )
+    list_filter = (
+        'email',
+    )
+    search_fields = (
+        'email',
+    )
+
+
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = (
+        'title',
+        'description',
+    )
+    list_display_links = (
+        'title',
+    )
+    list_filter = (
+        'title',
+    )
+    search_fields = (
+        'title',
+    )
+    ordering = (
+        'title',
+    )
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = (
+        'title',
+    )
+    list_display_links = (
+        'title',
+    )
+    list_filter = (
+        'title',
+    )
+    search_fields = (
+        'title',
+    )
+    ordering = (
+        'title',
+    )
+
+
+@admin.register(ServicesCategory)
+class ServicesCategoryAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = (
+        'title',
+    )
+    list_display_links = (
+        'title',
+    )
+    list_filter = (
+        'title',
+    )
+    search_fields = (
+        'title',
+    )
+    ordering = (
+        'title',
+    )
+
+
+@admin.register(OurServices)
+class OurServicesAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = (
+        'title',
+        'description',
+    )
+    list_display_links = (
+        'title',
+    )
+    list_filter = (
+        'title',
+    )
+    search_fields = (
+        'title',
+    )
+    ordering = (
+        'title',
+    )
+
+
--- apps/cleaning/models.py
+++ apps/cleaning/models.py
@@ -1,3 +1,130 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Contact(models.Model):
+    email = models.EmailField(
+        _('Email'),
+        max_length=50,
+        null=True,
+        blank=True,
+    )
+    instagram = models.URLField(
+        _('Instagram'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+    tiktok = models.URLField(
+        _('TikTok'),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    whatsapp = models.URLField(
+        _('WhatsApp'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+    telegram = models.URLField(
+        _('Telegram'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+    phone_number = models.CharField(
+        _('Номер тел.'),
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    address = models.URLField(
+        _('Адрес'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = _('Контакт')
+        verbose_name_plural = _('Контакты')
+
+    def __str__(self):
+        return self.email
+
+
+class AboutUs(models.Model):
+    title = models.CharField(
+        _('Заголовок'),
+        max_length=255,
+    )
+    description = models.TextField(
+        _('Описание'),
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('О нас')
+        verbose_name_plural = _('О нас')
+
+
+class Reviews(models.Model):
+    title = models.CharField(
+        _('Заголовок'),
+        max_length=255,
+    )
+    file = models.FileField(
+        _('Файл'),
+        upload_to='reviews/',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Отзывы')
+        verbose_name_plural = _('Отзывы')
+
+
+class ServicesCategory(models.Model):
+    title = models.CharField(
+        _('Заголовок'),
+        max_length=255,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Категория услуг')
+        verbose_name_plural = _('Категории услуг')
+
+
+class OurServices(models.Model):
+    category = models.ForeignKey(
+        ServicesCategory,
+        on_delete=models.CASCADE,
+        verbose_name=_('Категория услуг'),
+        related_name='services',
+    )
+    title = models.CharField(
+        _('Заголовок'),
+        max_length=255,
+    )
+    description = models.TextField(
+        _('Описание'),
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Наши услуги')
+        verbose_name_plural = _('Наши услуги')
+
