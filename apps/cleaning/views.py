edaba18303da73ebf8e2d82e1248b21e861fc7fe
Dastan-Oskonbaev <dastiw1910@gmail.com>
2023-10-26 13:56:44 +0600

    Added views and urls

--- apps/cleaning/views.py
+++ apps/cleaning/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Contact, AboutUs, OurServices, ServicesCategory, Reviews
+
+
+class IndexView(View):
+    def get(self, request):
+        contact = Contact.objects.all()
+        about = AboutUs.objects.all()
+        services = OurServices.objects.all()
+        category = ServicesCategory.objects.all()
+        reviews = Reviews.objects.all().first(4)
+
+        context = {
+            'title': 'Главная страница',
+            'contact': contact,
+            'about': about,
+            'reviews': reviews,
+            'category': category,
+            'services': services,
+        }
+
+        if about:
+            description = about.description.split("/")
+            context['description'] = description
+
+        if services:
+            service = services.description.split("/")
+            context['service'] = service
+
+        return render(request, 'cleaning/index.html', context)
+
