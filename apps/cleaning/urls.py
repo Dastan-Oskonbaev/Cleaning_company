eebb00249b2af88b67208c4025c9e8e1fa62b3f7
Janelaxagh <hell.agbsus@gmail.com>
2023-11-18 19:05:01 +0600

    add new urls for sitemap and robots txt

--- apps/cleaning/urls.py
+++ apps/cleaning/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, RobotsTxtView, SitemapXmlView
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
+    path('robots.txt', RobotsTxtView.as_view()),
+    path('sidemapxml.html', SitemapXmlView.as_view())
 ]
